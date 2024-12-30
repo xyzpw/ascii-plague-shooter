4162439b811c81ea80fcbe18a172ab169e401628
xyzpw <76017734+xyzpw@users.noreply.github.com>
2024-12-29 20:55:26 -0500

    feat: add comparison operator for position and mark equality operators as const

--- include/position.h
+++ include/position.h
@@ -4,10 +4,16 @@ struct Position {
     int column;
     int row;
 
-    bool operator==(const Position& other) {
+    bool operator==(const Position& other) const {
         return column == other.column && row == other.row;
     }
-    bool operator!=(const Position& other) {
+    bool operator!=(const Position& other) const {
         return column != other.column || row != other.row;
+    }
+    bool operator<(const Position& other) const {
+        if (column != other.column){
+            return column < other.column;
+        }
+        return row < other.row;
     }
 };
